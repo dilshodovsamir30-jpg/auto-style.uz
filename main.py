2cf18d233c012d324965ecb0864c7e21a63c99ea
dilshodovsamir30-jpg <dilshodovsamir30@gmail.com>
2026-02-27 22:31:12 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -2,24 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-
 @app.route("/")
 def home():
-     return render_template("index.html")
+    return render_template("index.html")
 
 @app.route("/about")
-def contact():
-     return render_template("about.html")
+def about():
+    return render_template("about.html")
 
 @app.route("/login")
 def login():
-     return render_template("login.html")
+    return render_template("login.html")
 
 @app.route("/register")
 def register():
-     return render_template("register.html")
+    return render_template("register.html")
 
-
-
-# app.run(debug=True, host="127.0.0.1")
+if __name__ == "__main__":
+    app.run(debug=True)
